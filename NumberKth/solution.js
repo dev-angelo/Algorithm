8e77f14e52e2841d43ed5025e1c401892d2728c1
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-01-15 10:33:43 +0900

    Modify variable scope
    command 가 for 문 내부에서 scope 를 갖도록 수정.

--- NumberKth/solution.js
+++ NumberKth/solution.js
@@ -1,9 +1,8 @@
 function solution(array, commands) {
     let answer = [];
-    let command = [];
 
     for (let index = 0; index < commands.length; index++) {
-        command = commands[index];
+        const command = commands[index];
         const sortedArray = array.slice(command[0]-1, command[1]).sort();
         answer.push(sortedArray[command[2]-1]);
     }
